f1469bee2d7a13979ae40231c602b704770319bc
Aleksandr <166635949+Mozyaa@users.noreply.github.com>
2024-06-20 15:36:46 +0300

    Add files via upload

---
+++ Attrib user AD/replace attrib user AD.ps1
@@ -0,0 +1,44 @@
+﻿
+# выбрать CSV-файл (SamAccountName;Title;Managerlogin;Department........ )
+$csvFilePath = ".\файл CSV"
+
+$data = Import-Csv -Path $csvFilePath -Delimiter ';'
+
+# Перебираем строку в CSV-файле
+foreach ($row in $data) {
+    $samAccountName = $row.SamAccountName
+    $Title = $row.Title
+    $Manager = $row.Manager
+    $Department = $row.Department
+    $PhysicalDeliveryOfficeName = $row.Office
+    $OfficePhone = $row.OfficePhone
+    $Mobile = $row.Mobile
+    $Company = $row.Company
+    $City = $row.City
+
+
+
+    
+    # Пытаемся получить учетную запись пользователя по SamAccountName
+    $user = Get-ADUser -Filter { SamAccountName -eq $samAccountName }
+    
+    # Если пользователь найден, обновляем атрибуты
+    if ($user) {
+        Set-ADUser -Identity $user -Replace @{
+            Title = $Title
+            Manager = (Get-ADUser -Filter { SamAccountName -eq $Manager }).DistinguishedName
+            Department = $Department
+            PhysicalDeliveryOfficeName = $PhysicalDeliveryOfficeName
+            telephoneNumber = $OfficePhone
+            Mobile = $Mobile
+            Company = $Company
+            l = $City
+        }
+        
+        Write-Host "Успешно обновлены атрибуты для пользователя: $samAccountName"
+    } else {
+        Write-Host "Пользователь с SamAccountName '$samAccountName' не найден"
+    }
+}
+
+Write-Host "Готово."
---
+++ cmdb/cmdb.ps1
@@ -0,0 +1,68 @@
+﻿# Скрипт собирает информацию из ru052\Users\Computer и Chekcfg данные за последние 6 месяцев.
+
+Remove-Item -Path ".\cmdb_old.csv" -Force
+Rename-Item -Path ".\cmdb.csv" -NewName "cmdb_old.csv" -Force
+
+$path = "\\ru052\Users\Computer"
+$dcheckcfg = "\\ru052\app$\Chekcfg\Data"
+
+
+$files = Get-ChildItem -Path $path -Filter *.csv | Where-Object { $_.LastWriteTime -ge (Get-Date).AddMonths(-6) }
+
+foreach ($file in $files) {
+  
+    $data = Get-Content $file.FullName
+
+    $lastWriteTime = $file.LastWriteTime
+
+    $fileName = $file.Name -replace '\.csv$'
+
+    $lastRow = $data[-1]
+ 
+    $thirdValue = $lastRow.Split(";")[2]
+    $LastLogin = $lastRow.Split(";")[3]
+    $LastLogindate = $lastRow.Split(";")[0]
+
+    $filePath = Get-ChildItem -Path $dcheckcfg -Recurse | Where-Object { $_.Name -eq $thirdValue } | Select-Object -ExpandProperty FullName
+    if ($filePath) {
+
+        $fileContent = Get-Content $filePath
+   $cpu = $fileContent | Where-Object { $_ -match '^cpu=' }
+        $cpuValue = if ($cpu -match 'CPU (.*)') {
+        $matches[1]
+        }
+
+   $memory = $fileContent | Where-Object { $_ -match '^Memory_in' }
+        $memoryValue = if ($memory -match 'Memory_in_Mb=(\d+)') {
+        [math]::Round($matches[1] / 1000, 2).ToString() + " GB" 
+        #$matches[1]
+        }
+
+   $hdd = $fileContent | Where-Object { $_ -match '^Total_HDD_in_Mb=' }
+        $hddValue = if ($hdd -match '^Total_HDD_in_Mb=(\d+)') {
+        [math]::Round($matches[1] / 1000, 2).ToString() + " GB" 
+        }
+
+   $ip = $fileContent | Where-Object { $_ -match '^IP_Addr=' }
+        $ipValue = if($ip -match 'IP_Addr=(.+?) Host:') {
+        $matches[1]
+        }
+
+   $recorddate = $fileContent | Where-Object { $_ -match '^Record_Date=' }
+        $recorddateValue = if($recorddate -match 'Record_Date=(.*)') {
+        $matches[1]
+        }
+
+   $os = $fileContent | Where-Object { $_ -match '^System=' }
+        $osValue = if($os -match 'System=(.*)') {
+        $matches[1]
+        }  
+
+  $monitormodel =  $fileContent | Where-Object { $_ -match '=Monitor' -and $_ -notmatch "Disabl" -and $_ -notmatch "Win_Device"}
+
+  $hddmodel = $fileContent | Where-Object { $_ -match '=HDD' }
+
+    Add-Content -Path ".\cmdb.csv" -Value ($LastLogin + ";" + $thirdValue + ";" + $cpuValue + ";" + $memoryValue + ";" + $hddValue + ";" + $ipValue + ";" + $osValue + ";" + $monitormodel + ";" + $hddmodel + ";" + $recorddateValue + ";" + $LastLogindate)
+
+       } 
+    }
