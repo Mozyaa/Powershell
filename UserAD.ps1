5abf2a58a341cc3d1df57de2acb5fa0f40279cab
Aleksandr <166635949+Mozyaa@users.noreply.github.com>
2024-06-20 15:36:30 +0300

    Add files via upload

---
+++ UserAD.ps1
@@ -0,0 +1,11 @@
+﻿# Скрипт собирает информацию обо всех пользователях в AD.
+
+$properties = @(
+    'samaccountname', 'name', 'Description', 'Title', 'Manager', 'Department', 
+    'office', 'OfficePhone', 'MobilePhone', 'mail', 'company', 'city', 
+    'StreetAddress', 'whenCreated', 'PasswordLastSet', 'Enabled', 'ThumbnailPhoto'
+)
+
+Get-ADUser -Filter * -Properties $properties | 
+Select-Object $properties | 
+Export-Csv -Path ".\alluserattrib.csv" -NoTypeInformation -Encoding utf8 -Delimiter ";"
